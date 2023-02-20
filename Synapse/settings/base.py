643ba1ae73c072ee60ff43d596ec9b08e9907862
Aakash Basnet <iamaakashbasnet@gmail.com>
2023-02-20 19:38:06 +0545

    chore: default user avatar

--- Synapse/settings/base.py
+++ Synapse/settings/base.py
@@ -71,7 +71,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/media/'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
 
 INTERNAL_IPS = [
     '127.0.0.1',
